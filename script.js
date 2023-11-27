e796ba83ad26bd4a7c8dfdcc792bc5ccbb884cfe
Ingrid Lira <gridferreiradoa2109@gmail.com>
2023-11-27 20:12:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".slide-video", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 800,
+    pagination: {
+      el: " .s-videos .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".s-videos .btn-next",
+        prevEl: ".s-videos .btn-prev",
+     },
+
+    breakpoints: {
+     320: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+     },
+
+     767: {
+      slidesPerView: 2.1,
+      spaceBetween: 16,
+     },
+
+     1024: {
+      slidesPerView: 3.1,
+      spaceBetween: 16,
+     },
+
+     1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+     }
+    }
+  });
